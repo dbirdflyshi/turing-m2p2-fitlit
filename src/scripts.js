d7f4d13e66cb92eeb318a1d3765b3b7164e6f942
dbirdflyshi <dander92@me.com>
2022-05-15 21:51:53 -0400

    complete up to task 11

--- src/scripts.js
+++ src/scripts.js
@@ -16,35 +16,83 @@ import './images/sleep.png'
 import './images/logo.png'
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Build User Repository 👇
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Creating the repo skeleton
-var userClasses = [];
-// converting the object array to a class instance of User array
-userData.map((user) => {
-    userClasses.push(new User(user))
-})
-// Creating the class instance of UserRepository object
-const userRepo = new UserRepository(userClasses);
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // query selector variables go here 👇
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+var userCard = document.querySelector('#userData')
+var waterButton = document.querySelector('#water');
+var exerciseButton = document.querySelector('#water');
+var sleepButton = document.querySelector('#water');
+var userList = document.querySelector('#users');
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // event listeners go here 👇
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//waterButton.addEventListener('click', outputSomething);
+
+document.getElementById("users").onchange = chooseUser;
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Global Variables Go here 👇
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+const userRepo = buildRepo();
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // functions and event handlers go here 👇
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+loadUserData(1);
 
+loadUserListDropdown();
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Build User Repository 👇
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function buildRepo(){
+    // Creating the repo skeleton
+    var userClasses = [];
+    // converting the object array to a class instance of User array
+    userData.map((user) => {
+        userClasses.push(new User(user))
+    })
+    // Creating the class instance of UserRepository object
+    return new UserRepository(userClasses);
+};
+
+function getStepGoalAvg(){
+    var total = userRepo.users.reduce((acc,user) =>{
+        acc += user.dailyStepGoal;
+        return acc;
+    },0)
+    var userCount = userRepo.users.length;
+    var avg = total / userCount
+    return avg;
+}
+
+function loadUserData(id){
+    var user = userRepo.users.find((user) => user.id === id);
+    console.log(user)
+    var stepGoalRating;
+    user.dailyStepGoal > getStepGoalAvg() ? stepGoalRating = 'Above Average' : stepGoalRating = 'Above Average'
+    
+    userCard.innerHTML = `<ul id="userData">
+                    <li>Name :<span style=\"color:rgba(230, 196, 157, 1)\"> ${user.name}</span></li>
+                    <li>Address :<span style=\"color:rgba(230, 196, 157, 1)\">  ${user.address}</span></li>
+                    <li>Email :<span style=\"color:rgba(230, 196, 157, 1)\">  ${user.email}</span></li>
+                    <li>Stride :<span style=\"color:rgba(230, 196, 157, 1)\">  ${user.strideLength}</span></li>
+                    <li>Step Goal :<span style=\"color:rgba(230, 196, 157, 1)\">  ${user.dailyStepGoal}</span></li>
+                    <li>Goal Rating:<span style=\"color:rgba(230, 196, 157, 1)\">  ${stepGoalRating}</span></li>
+                </ul>`
+}
+
+function loadUserListDropdown(){
+    var userSelectList ='';
+    userRepo.users.forEach((user) => {userSelectList+=`<option value=${user.id}>${user.name}</option>`});
+    userList.innerHTML = `<select name="users" id="users">${userSelectList}</select>`
+}
+
+function chooseUser() {
+    var selection = document.getElementById("users");
+    var option = parseInt(selection.options[selection.selectedIndex].value);
+    loadUserData(option);
+}
