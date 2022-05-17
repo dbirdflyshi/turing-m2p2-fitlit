b37eb538b751a2c51c1313367fedd3127da79d71
dbirdflyshi <dander92@me.com>
2022-05-17 18:56:35 -0400

    complete up to task 13

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,17 @@
-// Your fetch requests will live here!
-
-
-console.log('I will be a fetch request!')
-
+//Your fetch requests will live here!
+export let fetchUserData = () => {
+    return fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+    .then(response => response.json())
+}
+export let fetchSleepData = () => {
+    return fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
+        .then(response => response.json())
+}
+export let fetchHydrationData = () => {
+    return fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
+        .then(response => response.json())
+}
+export let fetchActivityData = () => {
+    return fetch("https://fitlit-api.herokuapp.com/api/v1/activity")
+        .then(response => response.json())
+}
--- src/scripts.js
+++ src/scripts.js
@@ -8,72 +8,110 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 var User = require('./User');
 var UserRepository = require('./UserRepository');
-const userData = require('./data/users');
 import './css/styles.css';
 import './images/water.png'
 import './images/exercise.png'
 import './images/sleep.png'
 import './images/logo.png'
+import{
+    fetchUserData,
+    fetchSleepData,
+    fetchHydrationData,
+    fetchActivityData
+} from './apiCalls';
+
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // query selector variables go here 👇
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 var userCard = document.querySelector('#userData')
+var userList = document.querySelector('#users');
+var friendsCard = document.querySelector('#friends-list')
 var waterButton = document.querySelector('#water');
 var exerciseButton = document.querySelector('#water');
 var sleepButton = document.querySelector('#water');
-var userList = document.querySelector('#users');
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// event listeners go here 👇
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//waterButton.addEventListener('click', outputSomething);
-
-document.getElementById("users").onchange = chooseUser;
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Global Variables Go here 👇
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const userRepo = buildRepo();
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// functions and event handlers go here 👇
+// Loads the function orchestrator 👇
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+window.addEventListener('load', loadAllData());
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// The main function that orchestrates everything 👇
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function loadAllData(){
+    Promise.all([fetchUserData(), 
+                 fetchSleepData(),
+                 fetchHydrationData(),
+                 fetchActivityData()
+    ]).then(data => {
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Build User Data 👇
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        const userDataList = buildRepo(data[0].userData);
+        //const userSleepData = buildRepo(data[1]);
+        const userHydrationData = data[2].hydrationData;
+        console.log(userHydrationData[0]);
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // event listeners 👇
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        //waterButton.addEventListener('click', outputSomething);
+        //document.getElementById("users").onchange = chooseUser;
+        document.getElementById("users").onchange = () => chooseUser(userDataList);
+
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Load User Info Sidebar 👇
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        loadUserData(userDataList, 1);
+
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Load User DropDown 👇
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        loadUserListDropdown(userDataList);
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Load Default Hydration 👇
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        //loadHydrationMetrics();
+    })
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// All functions that run in the orchestrator 👇
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-loadUserData(1);
 
-loadUserListDropdown();
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Build User Repository 👇
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-function buildRepo(){
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Creating the repo object 👇
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function buildRepo(data){
     // Creating the repo skeleton
     var userClasses = [];
     // converting the object array to a class instance of User array
-    userData.map((user) => {
-        userClasses.push(new User(user))
-    })
+    data.map((user) => userClasses.push(new User(user)))
     // Creating the class instance of UserRepository object
     return new UserRepository(userClasses);
 };
 
-function getStepGoalAvg(){
-    var total = userRepo.users.reduce((acc,user) =>{
-        acc += user.dailyStepGoal;
-        return acc;
-    },0)
-    var userCount = userRepo.users.length;
-    var avg = total / userCount
-    return avg;
-}
 
-function loadUserData(id){
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Loading User Info Card 👇
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function loadUserData(userRepo, id){
     var user = userRepo.users.find((user) => user.id === id);
-    console.log(user)
     var stepGoalRating;
-    user.dailyStepGoal > getStepGoalAvg() ? stepGoalRating = 'Above Average' : stepGoalRating = 'Above Average'
+    user.dailyStepGoal > getStepGoalAvg(userRepo) ? stepGoalRating = 'Above Average' : stepGoalRating = 'Above Average'
     
     userCard.innerHTML = `<ul id="userData">
                     <li>Name :<span style=\"color:rgba(230, 196, 157, 1)\"> ${user.name}</span></li>
@@ -83,16 +121,54 @@ function loadUserData(id){
                     <li>Step Goal :<span style=\"color:rgba(230, 196, 157, 1)\">  ${user.dailyStepGoal}</span></li>
                     <li>Goal Rating:<span style=\"color:rgba(230, 196, 157, 1)\">  ${stepGoalRating}</span></li>
                 </ul>`
+    loadFriends(userRepo, user);            
 }
 
-function loadUserListDropdown(){
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Calulating Avg Step Goal 👇
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function getStepGoalAvg(userRepo) {
+    var total = userRepo.users.reduce((acc, user) => {
+        acc += user.dailyStepGoal;
+        return acc;
+    }, 0)
+    var userCount = userRepo.users.length;
+    var avg = total / userCount
+    return avg;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Populating Friends List 👇
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function loadFriends(userRepo, user){
+    var friendIDs = user.friends
+    friendsCard.innerHTML = '';
+    friendIDs.forEach((friend) => {
+        var user = userRepo.users.find((user) => user.id === friend);
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(user.name));
+        friendsCard.appendChild(li);
+    })
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Populating User Drop Down 👇
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function loadUserListDropdown(userRepo){
     var userSelectList ='';
     userRepo.users.forEach((user) => {userSelectList+=`<option value=${user.id}>${user.name}</option>`});
     userList.innerHTML = `<select name="users" id="users">${userSelectList}</select>`
 }
 
-function chooseUser() {
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Load User From Drop Down Selection 👇
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function chooseUser(userDataList) {
     var selection = document.getElementById("users");
     var option = parseInt(selection.options[selection.selectedIndex].value);
-    loadUserData(option);
+    loadUserData(userDataList, option);
 }
+
